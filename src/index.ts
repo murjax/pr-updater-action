464d8f41d10e615c3c44c7ec8070ff59b2851286
Ryan Murphy <murjax@gmail.com>
2021-12-29 10:36:50 -0500

    Attempt updating github library

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 const token = core.getInput('token')
-const client = new github.GitHub(token)
+const octokit = github.getOctokit(token)
 
 async function main() {
     const baseBranch = github.context.payload.ref
-    const pullsResponse = await client.pulls.list({
+    const pullsResponse = await octokit.rest.pulls.list({
         ...github.context.repo,
         base: baseBranch,
         state: 'open',
@@ -14,7 +14,7 @@ async function main() {
     const prs = pullsResponse.data
     await Promise.all(
         prs.map((pr) => {
-            client.pulls.updateBranch({
+            octokit.rest.pulls.updateBranch({
                 ...github.context.repo,
                 pull_number: pr.number,
             })
